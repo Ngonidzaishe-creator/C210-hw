5426e249a182e3192e6cdd74edb282c9a1867fb6
Ngonidzaishe-creator <ngonidzaishegodfreychareka@gmail.com>
2025-09-08 16:47:55 +0200

    push

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -7,3 +7,36 @@ class Program
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
     }
 }
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> fruits = new List<string>();
+        string input;
+
+        Console.WriteLine("Enter fruits (type 'done' to finish):");
+        while (true)
+        {
+            input = Console.ReadLine();
+            if (input.ToLower() == "done")
+                break;
+            fruits.Add(input);
+        }
+
+        Console.WriteLine("You entered the following fruits:");
+        foreach (string fruit in fruits)
+        {
+            Console.WriteLine(fruit);
+        }
+
+        // Additional functionality: Sorting the list
+        fruits.Sort();
+        Console.WriteLine("Fruits in alphabetical order:");
+        foreach (string fruit in fruits)
+        {
+            Console.WriteLine(fruit);
+        }
+    }
+}
