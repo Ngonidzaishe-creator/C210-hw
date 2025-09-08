8b689234e02b65ab37c1f1c9086b678c237a1dc4
Ngonidzaishe-creator <ngonidzaishegodfreychareka@gmail.com>
2025-09-08 16:44:43 +0200

    push

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -7,3 +7,43 @@ class Program
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
     }
 }
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter your score (0-100): ");
+        int score = int.Parse(Console.ReadLine());
+
+        if (score >= 90)
+        {
+            Console.WriteLine("You received an A!");
+        }
+        else if (score >= 80)
+        {
+            Console.WriteLine("You received a B!");
+        }
+        else if (score >= 70)
+        {
+            Console.WriteLine("You received a C!");
+        }
+        else if (score >= 60)
+        {
+            Console.WriteLine("You received a D!");
+        }
+        else
+        {
+            Console.WriteLine("You received an F.");
+        }
+
+        // Additional feedback
+        if (score >= 60)
+        {
+            Console.WriteLine("Congratulations! You passed.");
+        }
+        else
+        {
+            Console.WriteLine("Don't worry, you can try again!");
+        }
+    }
+}
