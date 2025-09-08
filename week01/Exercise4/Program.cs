bab722552ba0c39e4091159f486713f5ca7edfb5
Ngonidzaishe-creator <ngonidzaishegodfreychareka@gmail.com>
2025-09-08 16:53:03 +0200

    push

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,12 +1,4 @@
 using System;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
-    }
-}
 using System.Collections.Generic;
 
 class Program
